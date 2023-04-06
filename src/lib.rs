cc72ec585e25d931bbcf792ecebd046e67a7af9d
Jondolf <joona.m.aalto@outlook.com>
2023-04-06 21:12:59 +0300

    Add crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+//! # Bevy XPBD
+//!
+//! **Bevy XPBD** is a 2D and 3D physics engine based on *Extended Position Based Dynamics* (XPBD) for the [Bevy game engine](https://bevyengine.org/). The *Entity Component System* (ECS) is used heavily throughout the engine to enable enhanced parallellism, configurability and familiarity, while making the engine fit better into the Bevy ecosystem.
+//!
+//! XPBD is an improved variant of traditional *Position Based Dynamics* (PBD).
+//! It provides unconditionally stable, time step independent and physically accurate simulations that use simple constraint projection to handle things like contacts, joints, and interactions between rigid bodies, soft bodies and fluids.
+//!
+//! To understand the algorithm better, it's worth checking out some of the academic papers:
+//!
+//! - Müller M, Macklin M, Chentanez N, Jeschke S, Kim T. 2020. *[Detailed Rigid Body Simulation with Extended Position Based Dynamics](https://matthias-research.github.io/pages/publications/PBDBodies.pdf)*.
+//!
+//! - Macklin M, Müller M, Chentanez N. 2016. *[XPBD: Position-Based Simulation of Compliant Constrained Dynamics](http://mmacklin.com/xpbd.pdf)*.
+
 #[cfg(all(feature = "f32", feature = "f64"))]
 compile_error!("feature \"f32\" and feature \"f64\" cannot be enabled at the same time");
 
