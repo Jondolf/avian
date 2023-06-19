21901b9bd59a10c3b53a7b74b8901667625c164e
Jondolf <jondolf.dev@gmail.com>
2023-06-19 20:54:24 +0300

    Add `publish.sh` script

---
+++ publish.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+tmp=$(mktemp -d)
+
+echo "$tmp"
+currdir=$(pwd)
+
+cp -r src "$tmp"/.
+cp -r LICENSE-MIT LICENSE-APACHE README.md "$tmp"/.
+
+### Publish bevy_xpbd_2d
+sed 's#\.\./\.\./src#src#g' crates/bevy_xpbd_2d/Cargo.toml > "$tmp"/Cargo.toml
+cp -r crates/bevy_xpbd_2d/examples "$tmp"/.
+cd "$tmp" && cargo publish --dry-run
+
+### Remove the 2D examples and return to previous directory
+rm -rf examples
+cd "$currdir" || exit
+
+### Publish bevy_xpbd_3d
+sed 's#\.\./\.\./src#src#g' crates/bevy_xpbd_3d/Cargo.toml > "$tmp"/Cargo.toml
+cp -r crates/bevy_xpbd_3d/examples "$tmp"/.
+cd "$tmp" && cargo publish --dry-run
+
+rm -rf "$tmp"
