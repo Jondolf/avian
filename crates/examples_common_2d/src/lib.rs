524ffde6bd8b2ba06459c0302f2e2b308023d290
Jondolf <jondolf.dev@gmail.com>
2023-07-10 00:27:40 +0300

    Add FPS counter for examples

--- crates/examples_common_2d/src/lib.rs
+++ crates/examples_common_2d/src/lib.rs
@@ -1,4 +1,8 @@
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+    text::DEFAULT_FONT_HANDLE,
+};
 use bevy_xpbd_2d::prelude::*;
 
 #[derive(Default)]
@@ -6,10 +10,12 @@ pub struct XpbdExamplePlugin;
 
 impl Plugin for XpbdExamplePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(PhysicsPlugins::default())
+        app.add_plugins((PhysicsPlugins::default(), FrameTimeDiagnosticsPlugin))
             .add_state::<AppState>()
+            .add_systems(Startup, setup)
             .add_systems(OnEnter(AppState::Paused), bevy_xpbd_2d::pause)
             .add_systems(OnExit(AppState::Paused), bevy_xpbd_2d::resume)
+            .add_systems(Update, update_fps_text)
             .add_systems(Update, pause_button)
             .add_systems(Update, step_button.run_if(in_state(AppState::Paused)));
     }
@@ -39,5 +45,39 @@ fn pause_button(
 fn step_button(mut physics_loop: ResMut<PhysicsLoop>, keys: Res<Input<KeyCode>>) {
     if keys.just_pressed(KeyCode::Return) {
         physics_loop.step();
+    }
+}
+
+#[derive(Component)]
+struct FpsText;
+
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        TextBundle::from_section(
+            "FPS: ",
+            TextStyle {
+                font: DEFAULT_FONT_HANDLE.typed(),
+                font_size: 20.0,
+                color: Color::TOMATO,
+            },
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            top: Val::Px(5.0),
+            left: Val::Px(5.0),
+            ..default()
+        }),
+        FpsText,
+    ));
+}
+
+fn update_fps_text(diagnostics: Res<DiagnosticsStore>, mut query: Query<&mut Text, With<FpsText>>) {
+    for mut text in &mut query {
+        if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(value) = fps.smoothed() {
+                // Update the value of the second section
+                text.sections[0].value = format!("FPS: {value:.2}");
+            }
+        }
     }
 }
