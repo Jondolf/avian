f2f8aa58455955a34c6f4bfe4a795963368ba743
ShaddowSpy <shaddowspy@gmail.com>
2023-08-28 16:04:40 -0600

    Fix child Transform in position_to_transform

--- src/plugins/sync.rs
+++ src/plugins/sync.rs
@@ -245,7 +245,7 @@ fn position_to_transform(
                 // The new local transform of the child body,
                 // computed from the its global transform and its parents global transform
                 let new_transform = GlobalTransform::from(
-                    Transform::from_translation(pos.as_f32().extend(transform.translation.z))
+                    Transform::from_translation(pos.as_f32().extend(0.0))
                         .with_rotation(Quaternion::from(*rot).as_f32()),
                 )
                 .reparented_to(&GlobalTransform::from(parent_transform));
