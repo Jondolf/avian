eb0da78eb485de43c9bd21fcdc5bcd4760eb0175
Jondolf <jondolf.dev@gmail.com>
2023-10-07 22:02:22 +0300

    Fix doc link

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@
 //!     - Access to [colliding entities](CollidingEntities)
 //!     - [Sensor colliders](Sensor)
 //!     - [Collision layers](CollisionLayers)
-//!     - [Contact and time of impact queries](narrow_phase::query)
+//!     - [Contact and time of impact queries](narrow_phase::contact_query)
 //! - Material properties like [restitution](Restitution) and [friction](Friction)
 //! - [Linear damping](LinearDamping) and [angular damping](AngularDamping) for simulating drag
 //! - [Gravity] and [gravity scale](GravityScale)
