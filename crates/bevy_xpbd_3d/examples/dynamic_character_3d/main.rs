8ed83dc0bf4a9a45443f50b71471479c8b140f9b
Jondolf <jondolf.dev@gmail.com>
2023-11-04 22:35:58 +0200

    Add 2D versions of character controller examples

--- crates/bevy_xpbd_3d/examples/dynamic_character_3d/main.rs
+++ crates/bevy_xpbd_3d/examples/dynamic_character_3d/main.rs
@@ -1,16 +1,15 @@
-//! A basic implementation of a character controller for a kinematic rigid body.
+//! A basic implementation of a character controller for a dynamic rigid body.
 //!
 //! This showcases the following:
 //!
 //! - Basic directional movement and jumping
 //! - Support for both keyboard and gamepad input
-//! - A configurable maximum slope angle
-//! - Collision response for kinematic bodies
+//! - A configurable maximum slope angle for jumping
 //! - Loading a platformer environment from a glTF
 //!
 //! The character controller logic is contained within the `plugin` module.
 //!
-//! For a dynamic character controller, see the `dynamic_character` example.
+//! For a kinematic character controller, see the `kinematic_character_3d` example.
 
 mod plugin;
 
@@ -46,8 +45,15 @@ fn setup(
             transform: Transform::from_xyz(0.0, 1.5, 0.0),
             ..default()
         },
-        CharacterControllerBundle::new(Collider::capsule(1.0, 0.4), Vector::NEG_Y * 9.81 * 2.0)
-            .with_movement(30.0, 0.92, 7.0, (30.0 as Scalar).to_radians()),
+        CharacterControllerBundle::new(Collider::capsule(1.0, 0.4)).with_movement(
+            30.0,
+            0.92,
+            7.0,
+            (30.0 as Scalar).to_radians(),
+        ),
+        Friction::ZERO.with_combine_rule(CoefficientCombine::Min),
+        Restitution::ZERO.with_combine_rule(CoefficientCombine::Min),
+        GravityScale(2.0),
     ));
 
     // A cube to move around
--- crates/bevy_xpbd_3d/examples/kinematic_character_3d/main.rs
+++ crates/bevy_xpbd_3d/examples/kinematic_character_3d/main.rs
@@ -1,15 +1,16 @@
-//! A basic implementation of a character controller for a dynamic rigid body.
+//! A basic implementation of a character controller for a kinematic rigid body.
 //!
 //! This showcases the following:
 //!
 //! - Basic directional movement and jumping
 //! - Support for both keyboard and gamepad input
-//! - A configurable maximum slope angle for jumping
+//! - A configurable maximum slope angle
+//! - Collision response for kinematic bodies
 //! - Loading a platformer environment from a glTF
 //!
 //! The character controller logic is contained within the `plugin` module.
 //!
-//! For a kinematic character controller, see the `kinematic_character` example.
+//! For a dynamic character controller, see the `dynamic_character_3d` example.
 
 mod plugin;
 
@@ -45,15 +46,8 @@ fn setup(
             transform: Transform::from_xyz(0.0, 1.5, 0.0),
             ..default()
         },
-        CharacterControllerBundle::new(Collider::capsule(1.0, 0.4)).with_movement(
-            30.0,
-            0.92,
-            7.0,
-            (30.0 as Scalar).to_radians(),
-        ),
-        Friction::ZERO.with_combine_rule(CoefficientCombine::Min),
-        Restitution::ZERO.with_combine_rule(CoefficientCombine::Min),
-        GravityScale(2.0),
+        CharacterControllerBundle::new(Collider::capsule(1.0, 0.4), Vector::NEG_Y * 9.81 * 2.0)
+            .with_movement(30.0, 0.92, 7.0, (30.0 as Scalar).to_radians()),
     ));
 
     // A cube to move around
