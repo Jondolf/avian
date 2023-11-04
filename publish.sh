7bdc3e44b0d438f877b90f8f8d5a663292fed0d2
Joona Aalto <jondolf.dev@gmail.com>
2023-11-05 00:14:40 +0200

    Copy benches and assets in `publish.sh`

--- publish.sh
+++ publish.sh
@@ -11,7 +11,7 @@ cp -r LICENSE-MIT LICENSE-APACHE README.md "$tmp"/.
 ### Publish bevy_xpbd_2d
 sed 's#\.\./\.\./src#src#g' crates/bevy_xpbd_2d/Cargo.toml > "$tmp"/Cargo.toml
 cp -r crates/bevy_xpbd_2d/examples "$tmp"/.
-cd "$tmp" && cargo publish --dry-run
+cd "$tmp" && cargo publish
 
 ### Remove the 2D examples and return to previous directory
 rm -rf examples
@@ -19,7 +19,9 @@ cd "$currdir" || exit
 
 ### Publish bevy_xpbd_3d
 sed 's#\.\./\.\./src#src#g' crates/bevy_xpbd_3d/Cargo.toml > "$tmp"/Cargo.toml
+cp -r crates/bevy_xpbd_3d/assets "$tmp"/.
 cp -r crates/bevy_xpbd_3d/examples "$tmp"/.
-cd "$tmp" && cargo publish --dry-run
+cp -r crates/bevy_xpbd_3d/benches "$tmp"/.
+cd "$tmp" && cargo publish
 
 rm -rf "$tmp"
