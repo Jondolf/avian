918e0feb513c0ea9d74a75e19c5402bb02ea6d8f
Joshua Pedrick <joshua.pedrick@gmail.com>
2024-02-28 17:45:48 -0500

    Add feature to examples_common to enable PhysicsDebugPlugin

--- crates/examples_common_3d/src/lib.rs
+++ crates/examples_common_3d/src/lib.rs
@@ -11,20 +11,25 @@ pub struct XpbdExamplePlugin;
 
 impl Plugin for XpbdExamplePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((PhysicsPlugins::default(), FrameTimeDiagnosticsPlugin))
-            .init_state::<AppState>()
-            .add_systems(Startup, setup)
-            .add_systems(
-                OnEnter(AppState::Paused),
-                |mut time: ResMut<Time<Physics>>| time.pause(),
-            )
-            .add_systems(
-                OnExit(AppState::Paused),
-                |mut time: ResMut<Time<Physics>>| time.unpause(),
-            )
-            .add_systems(Update, update_fps_text)
-            .add_systems(Update, pause_button)
-            .add_systems(Update, step_button.run_if(in_state(AppState::Paused)));
+        app.add_plugins((
+            PhysicsPlugins::default(),
+            FrameTimeDiagnosticsPlugin,
+            #[cfg(feature = "use-debug-plugin")]
+            PhysicsDebugPlugin::default(),
+        ))
+        .init_state::<AppState>()
+        .add_systems(Startup, setup)
+        .add_systems(
+            OnEnter(AppState::Paused),
+            |mut time: ResMut<Time<Physics>>| time.pause(),
+        )
+        .add_systems(
+            OnExit(AppState::Paused),
+            |mut time: ResMut<Time<Physics>>| time.unpause(),
+        )
+        .add_systems(Update, update_fps_text)
+        .add_systems(Update, pause_button)
+        .add_systems(Update, step_button.run_if(in_state(AppState::Paused)));
     }
 }
 
