c207fd4c8ebf7a68db6d72c26e7863761954a438
Doonv <58695417+doonv@users.noreply.github.com>
2024-03-11 12:34:48 +0200

    Use `compile_error!` macro instead of panicking in `PhysicsLayer` derive macro (#347)
    # Objective
    
    Improve errors in the `PhysicsLayer` derive macro
    
    ## Solution
    
    Instead of panicking, we can instead return a `compile_error!` macro. This removes the "proc-macro panicked" message and also allows us to specify a span (location in the code) for the error.
    
    ### Future solutions
    
    I'm thinking of also adding a `nightly` feature which uses the unstable diagnostic API for even better errors.
    
    ---
    
    ## Changelog
    
    - Improved errors in the `PhysicsLayer` derive macro

--- crates/bevy_xpbd_derive/src/lib.rs
+++ crates/bevy_xpbd_derive/src/lib.rs
@@ -2,8 +2,8 @@
 
 use proc_macro::TokenStream;
 
-use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput};
+use quote::{quote, quote_spanned};
+use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput};
 
 // Modified macro From the discontinued Heron,
 // see https://github.com/jcornaz/heron/blob/main/macros/src/lib.rs
@@ -12,22 +12,43 @@ pub fn derive_physics_layer(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let enum_ident = input.ident;
 
+    fn non_enum_item_error(span: proc_macro2::Span) -> TokenStream {
+        quote_spanned! { span =>  compile_error!("only enums can automatically derive PhysicsLayer"); }
+                .into()
+    }
     let variants = match &input.data {
         Data::Enum(data) => &data.variants,
-        _ => panic!("Only enums can automatically derive PhysicsLayer"),
+        Data::Struct(data) => {
+            return non_enum_item_error(data.struct_token.span);
+        }
+        Data::Union(data) => {
+            return non_enum_item_error(data.union_token.span);
+        }
     };
 
-    assert!(variants.len() <= 32, "Reached the maximum of 32 layers");
+    if variants.len() > 32 {
+        return quote! { compile_error!("PhysicsLayer only supports a maximum of 32 layers"); }
+            .into();
+    }
 
-    let to_bits = variants.iter().enumerate().map(|(index, variant)| {
-        let bits: u32 = 1 << index;
-        assert!(
-            variant.fields.is_empty(),
-            "Can only derive PhysicsLayer for enums without fields"
-        );
-        let ident = &variant.ident;
-        quote! { #enum_ident::#ident => #bits, }
-    });
+    let to_bits_result: Result<Vec<_>, _> = variants
+        .iter()
+        .enumerate()
+        .map(|(index, variant)| {
+            if !variant.fields.is_empty() {
+                return Err(variant.fields.span());
+            }
+            let bits: u32 = 1 << index;
+            let ident = &variant.ident;
+
+            Ok(quote! { #enum_ident::#ident => #bits, })
+        })
+        .collect();
+
+    let to_bits = match to_bits_result {
+        Ok(tokens) => tokens,
+        Err(span) => return quote_spanned! { span => compile_error!("can only derive PhysicsLayer for enums without fields"); }.into(),
+    };
 
     let all_bits: u32 = if variants.len() == 32 {
         0xffffffff
