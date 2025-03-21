2b4089677731d7581694881456c2e36498b0dcfa
Joona Aalto <jondolf.dev@gmail.com>
2025-03-21 23:09:17 +0200

    Add debugdump notes for 3D too

--- crates/avian3d/examples/debugdump_3d.rs
+++ crates/avian3d/examples/debugdump_3d.rs
@@ -1,12 +1,13 @@
 //! Run with:
 //! `cargo run --example debugdump_3d > dump.dot  && dot -Tsvg dump.dot > dump.svg`
 
-/*
-use avian3d::debug_render::PhysicsDebugPlugin;
-use avian3d::prelude::*;
-use bevy::prelude::*;
+// use avian3d::prelude::*;
+// use bevy::prelude::*;
 
 fn main() {
+    todo!("`bevy_mod_debugdump` is not compatible with Bevy 0.16 yet. This example will be updated when it is.");
+
+    /*
     let mut app = App::new();
 
     app.add_plugins((PhysicsPlugins::default(), PhysicsDebugPlugin::default()));
@@ -17,7 +18,5 @@ fn main() {
     // - FixedPostUpdate
     // - Update
     bevy_mod_debugdump::print_schedule_graph(&mut app, PhysicsSchedule);
+    */
 }
-*/
-
-fn main() {}
