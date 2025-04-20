4f4a9cd419f5ae861381eb01c3e22408b5cbea4e
Joona Aalto <jondolf.dev@gmail.com>
2025-04-20 22:55:30 +0300

    Update transform hash

--- src/tests/determinism_2d.rs
+++ src/tests/determinism_2d.rs
@@ -62,7 +62,7 @@ fn cross_platform_determinism_2d() {
     let hash = compute_hash(app.world(), query);
 
     // Update this value if simulation behavior changes.
-    let expected = 0x333a3655;
+    let expected = 0x7c4e9de8;
 
     assert!(
         hash == expected,
