519dde044cab5f9de30d41001302751ad607e1d0
Joona Aalto <jondolf.dev@gmail.com>
2025-05-13 19:37:59 +0300

    Fix use of deprecated `with_compliance`

--- src/tests/determinism_2d.rs
+++ src/tests/determinism_2d.rs
@@ -142,7 +142,7 @@ fn setup_scene(mut commands: Commands) {
                     )),
                     RevoluteJoint::new(prev_entity.unwrap(), entity)
                         .with_angle_limits(-0.1 * PI, 0.2 * PI)
-                        .with_compliance(0.0001)
+                        .with_point_compliance(0.0001)
                         .with_local_anchor_1(Vec2::splat(half_size).adjust_precision())
                         .with_local_anchor_2(Vec2::new(offset, -half_size).adjust_precision()),
                 ));
