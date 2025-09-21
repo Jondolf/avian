33d252ce2041b49df57e44c4673f943a3d525fc5
Joona Aalto <jondolf.dev@gmail.com>
2025-09-21 15:54:53 +0300

    Move `delta_time` out of loop

--- crates/avian2d/examples/dynamic_character_2d/plugin.rs
+++ crates/avian2d/examples/dynamic_character_2d/plugin.rs
@@ -225,10 +225,11 @@ fn apply_movement_damping(
     time: Res<Time>,
     mut query: Query<(&MovementDampingFactor, &mut LinearVelocity)>,
 ) {
+    // Precision is adjusted so that the example works with
+    // both the `f32` and `f64` features. Otherwise you don't need this.
+    let delta_time = time.delta_secs_f64().adjust_precision();
+
     for (damping_factor, mut linear_velocity) in &mut query {
-        // Precision is adjusted so that the example works with
-        // both the `f32` and `f64` features. Otherwise you don't need this.
-        let delta_time = time.delta_secs_f64().adjust_precision();
         // We could use `LinearDamping`, but we don't want to dampen movement along the Y axis
         linear_velocity.x *= 1.0 / (1.0 + damping_factor.0 * delta_time);
     }
