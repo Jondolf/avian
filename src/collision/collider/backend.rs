6b8bab96ab46d958cd15c0b55c3cda010d926f59
Jan Hohenheim <jan@hohenheim.ch>
2025-09-29 20:36:09 +0200

    Remove unused code

--- src/collision/collider/backend.rs
+++ src/collision/collider/backend.rs
@@ -339,8 +339,6 @@ fn init_collider_constructors(
         let collider = Collider::try_from_constructor(constructor.clone());
 
         if let Some(collider) = collider {
-            use crate::collision::collider::constructor::ColliderConstructorReady;
-
             commands.entity(entity).insert(collider);
             commands.trigger(ColliderConstructorReady { entity })
         } else {
