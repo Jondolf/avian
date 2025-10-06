f4aa5e2d503c4615baa6b5b47f145522470ae628
Kristoffer Søholm <k.soeholm@gmail.com>
2025-10-06 15:35:57 +0200

    Bump bevy_mod_debugdump

--- crates/avian3d/examples/debugdump_3d.rs
+++ crates/avian3d/examples/debugdump_3d.rs
@@ -1,7 +1,6 @@
 //! Run with:
 //! `cargo run --example debugdump_3d > dump.dot  && dot -Tsvg dump.dot > dump.svg`
 
-/*
 use avian3d::prelude::*;
 use bevy::prelude::*;
 
@@ -16,9 +15,4 @@ fn main() {
     // - FixedPostUpdate
     // - Update
     bevy_mod_debugdump::print_schedule_graph(&mut app, PhysicsSchedule);
-}
-*/
-
-fn main() {
-    todo!("This example will be updated when bevy_mod_debugdump supports Bevy 0.17");
 }
